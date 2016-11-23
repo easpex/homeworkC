ffdc1f71b22b6153868ef820c80bfd8df527e850
easpex <subsinfoapps@gmail.com>
2016-11-23 18:29:11 +0200

    final ver

--- maman11/con.c
+++ maman11/con.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-#define LOWER 0 /*the integer value of an ascii char cannot be lower than 0 */
-#define HIGHER 127 /*the integer value of an ascii char cannot be higher than 127 */
 #define TWO_CHAR_STRING 2
 #define ONE_CHAR_STRING 1
-#define TRUE 1
-#define FALSE 0
-#define MIN_SEQUENCE 3
-#define TWO_STEPS 2
-#define INVALID 99
-#define ZEROETH 0
-#define FIRST 1
 #define INCREASED_BY_ONE 1
 
 void contract(char s1[], char s2[]);
@@ -28,18 +19,26 @@ return 0;
 }
 
 void contract(char s1[], char s2[]) {
-/*
-0. if the string is length 1 return the string
-1. convert char to int
-2. check that it's between 0 to 127
-3. init min/max
-4. if the order is increasing and increases only by 1 then continue and increment the max
-	else if (the order is the same or decreases)
-		write the min and the max in the "a-z" format
-	else if (the order has always been the same or decreasing)
-		keep writing the same string to s2
-
+	
+/*	arguments: char s1[] - array with character string
+			   char s2[] - empty array
+	return: void
+	description: the function receives 2 arrays. array s1 contains ASCII characters. The function shortens sequence(s) of
+	characters according to this format: "a-z" where 'a' is the char with the lowest lexigraphical value while 'z'
+	has the highest. if the string is only 2 chars long (increasing lexigraphical order) than we don't need the hyphen
+	in the format. if the order is not increasing lexigraphical then we just write the chars "as is" into the array s2.
+	At the end, s2 array will contain the result. The algorithm is the following:
+		1) use a while-loop with the condition that the iterator i is less than the string's length
+		2) check if we have a case of increasing lexigraphical order. if yes then increment str variable which stores
+		the length of the currect string with increasing lexigraphical order.
+		3) else if we have a case of just 2 chars of increasing lexigraphical order then write them to s2 array.
+		reset min and str.
+		4) else if we have a case of 3 or more chars on increasing lexigraphical order then shorten them (use the 
+		the minimal char and the max char and add a hyphen). reset min and str.
+		5) the last case is when we have a descending lexigraphical order. then we immediately write the char to
+		s2 array. reset min. 
 */
+
 int min; /* the first ascii letter in the current string with increasing order */
 int max; /* the ascii char with the highest integer value in the current string with increasing order */
 int i = 0; /* the index for traversing the s1 array */
@@ -54,43 +53,38 @@ min = s1[i]; /* set the min */
 while(i < str_length) {	 /* do the while loop so long as the current char is not end of the string */
 	max = s1[i]; /* set the new max. essentially we need a new max with each iteration */
 
-	/* check if max is higher than min exactly by one */
-	if(max - s1[i - 1] == INCREASED_BY_ONE) {
+	if(max - s1[i - 1] == INCREASED_BY_ONE) { /* check if max is higher than min exactly by one */
 		++str; /* we increment str to signify increasing order with increments of 1 occured */
 		++i;
-	} else if(str == TWO_CHAR_STRING) { /* we have a string with increasing order but it's only 2 chars
+	} else if(str == TWO_CHAR_STRING) { /* we have a string with increasing order but it's only 2 chars long
 	thus just write it to s2 array*/
-		printf("inside 2 if \n");
-		s2[j] = min; /* write the first char from the string */
-		s2[++j] = s1[i - 1]; /* write the second char from the string */
+		s2[j] = min; /* write the first char from the current string */
+		s2[++j] = s1[i - 1]; /* write the second char from the current string */
 		s2[++j] = '\0'; /* the last char in s2 needs to be '\0' to signify the end of the string */
-		str = ONE_CHAR_STRING; /* we took care of the current string with increasing order. now reset the str to 1 for new count */
+		str = ONE_CHAR_STRING; /* we took care of the current string with increasing order. 
+		now reset the str to 1 for new count */
 		min = s1[i]; /* now we need a new min */
 		++i;
 	} else if(str > TWO_CHAR_STRING){ /* we have a string with increasing order it's 3 chars or more and we need to
 	make a contraction in this case */
-		printf("inside 3 if \n");
-		s2[j] = min; /* min is the starting value of the current str */
-		s2[++j] = '-'; /* insert a hyphen according to the format */
+		s2[j] = min; /* min is the starting value of the current string */
+		s2[++j] = '-'; /* insert a hyphen according to the requested format */
 		s2[++j] = s1[i - 1]; /* s1[i - 1] is the highest value of the current str */
 		s2[++j] = '\0'; /* the last char in s2 needs to be '\0' to signify the end of the string */
-		str = ONE_CHAR_STRING;
+		str = ONE_CHAR_STRING; /* we took care of the current string with increasing order. 
+		now reset the str to 1 for new count */
 		min = max; /* now we need a new min */
 		++i;
-	} else if(str == ONE_CHAR_STRING && (max - s1[i - 1] != INCREASED_BY_ONE)) { /* if a string with decreasing order occured
-	or the integer interval between chars is not equal to +1 */
-		printf("inside 4 if \n");
-		s2[j] = min;
+	} else if(str == ONE_CHAR_STRING && (max - s1[i - 1] != INCREASED_BY_ONE)) { /* if a string with decreasing
+	order occured. we know this because either str was never incremented or the integer interval between max
+	and the previous char to max is not 1 */
+		s2[j] = min; /* write the current char to s2 string */
 		s2[++j] = '\0'; /* the last char in s2 needs to be '\0' to signify the end of the string */
 		min = max; /* now we need a new min */
 		++i;
-	}
-	printf("the i inside while = %d | the j = %d | s2 len = %d\n", i, j, strlen(s2));
-} /*end of while*/
-
-
-
-}
+	} /* end of if */
+} /* end of while */
+} /* end of contract */
 
 /* helper function, prints a char array */
 void printArr(char arr[]) {
